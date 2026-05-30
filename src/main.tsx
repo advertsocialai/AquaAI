d18d4698b0d08c41cccea82a4ea1ee8772bbbcb4
advertsocialai <advertsocialai@gmail.com>
2026-05-29 19:49:59 -0500

    Revert "Web: switch default theme to light + bump theme storage key"
    This reverts commit 2395208ce062373fca3121873ba95084f75b8b77.

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { initSentry } from "@/lib/sentry";
 initSentry();
 
 createRoot(document.getElementById("root")!).render(
-  <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} storageKey="aquarudra-theme">
+  <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} storageKey="aquai-theme">
     <App />
   </ThemeProvider>,
 );
